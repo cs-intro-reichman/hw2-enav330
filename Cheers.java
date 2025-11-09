895d02b3c1a32c59c4f8b58a282c6c38631e5a80
Enav Gefen <enav.gefen@post.runi.ac.il>
2025-11-09 16:36:13 +0200

    final

--- Cheers.java
+++ Cheers.java
@@ -4,7 +4,7 @@ public class Cheers {
         String cheerWord = args[0];
         int N = Integer.parseInt(args[1]);
         cheerWord = cheerWord.toUpperCase(); 
-        String specialChars = "AEFHILMNRSX"; 
+        String specialChars = "AEFHILMNROSX"; 
         for (int i = 0; i < cheerWord.length(); i++) {
             char currentChar = cheerWord.charAt(i);
             String article; 
@@ -14,7 +14,7 @@ public class Cheers {
                 article = "a";
             }
             
-            System.out.println("Give me " + article + " " + currentChar + ": " + currentChar + "!");
+            System.out.println("Give me " + article + "  " + currentChar + ": " + currentChar + "!");
         }
         
         System.out.println("What does that spell?");

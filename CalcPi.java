257bca24b75706e39accb3eda4dbdcb91362a206
Enav Gefen <enav.gefen@post.runi.ac.il>
2025-11-12 18:18:54 +0200

    final

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,16 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	 int N = Integer.parseInt(args[0]);
+	 double piapprox = 1.0;
+	 double num = 3;
+	 double sign = 1;
+	 for(int i = 1; i <= N - 1; i++){
+		piapprox = piapprox -((1/num)*(sign));
+		num = num + 2;
+		sign = sign *(-1);
+	 }
+	 System.out.println("pi acocording to java: " + Math.PI);
+	 System.out.println("pi, approximated:      " + piapprox * 4);
 	}
 }
--- Cheers.java
+++ Cheers.java
@@ -4,14 +4,10 @@ public class Cheers {
         String cheerWord = args[0];
         int N = Integer.parseInt(args[1]);
         
-        // 1. תיקון רשימת specialChars (התאמה לדרישת המבדק)
-        // הרשימה הנכונה עבור "an"
         String specialChars = "AEFHILMNROSX"; 
         
         cheerWord = cheerWord.toUpperCase(); 
-        
-        // 2. הלולאה הראשונה
-        for (int i = 0; i < cheerWord.length(); i++) {
+                for (int i = 0; i < cheerWord.length(); i++) {
             
             char currentChar = cheerWord.charAt(i);
             String article; 
@@ -19,18 +15,13 @@ public class Cheers {
             if (specialChars.indexOf(currentChar) != -1) {
                 article = "an";
             } else {
-                // נדרש רווח נוסף עבור "a" כדי להתאים לפורמט
                 article = "a "; 
             }
-            
-            // הדפסה: "Give me [a/an] [רווח] [תו]: "
             System.out.print("Give me " + article + " " + currentChar + ": "); 
             
-            // הדפסת התו עם סימן הקריאה וירידת שורה
             System.out.println(currentChar + "!");
         }
         
-        // 3. סיכום והדפסה חוזרת
         System.out.println("What does that spell?");
         
         for (int j = 0; j < N; j++) {
